712b89fa65745d0a53976b10e9a65ef2b165ad12
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2017-02-27 09:37:57 +0100

    Updated Vampirism: Doesn't cloak anymore Rebalanced hp steal values

---
+++ lua/GhoulsBalance/Vampirism/DamageTypes.lua
@@ -0,0 +1,7 @@
+kLifeformVampirismScalars = {} --FIXME change to Weapon/Doer classnames, not lifeform
+kLifeformVampirismScalars["Skulk"] = 14
+kLifeformVampirismScalars["Gorge"] = 18
+kLifeformVampirismScalars["LerkBite"] = 15
+kLifeformVampirismScalars["LerkSpikes"] = 6
+kLifeformVampirismScalars["Fade"] = 36
+kLifeformVampirismScalars["Onos"] = 60
